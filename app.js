204989238c8afbb1d7a7ee8d3a84da6b37725731
radin-reth <radin.reth@gmail.com>
2017-08-08 22:48:34 +0700

    basic jsx with babel

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
-var h1 = React.createElement('h1', { className: 'success' }, 'Hello World!');
-ReactDOM.render(h1, document.getElementById('app'));
-console.log(123);
+"use strict";
+
+// var h1 = React.createElement('h1', { "className": 'success' }, 'Hello World!');
+// ReactDOM.render(h1, document.getElementById('app'));
+ReactDOM.render(<h1>sdf</h1>, document.getElementById('app'));
