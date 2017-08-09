7592c555af7d815f636f04f1f96dd6e56aefa879
radin-reth <radin.reth@gmail.com>
2017-08-09 23:15:28 +0700

    create basic react component

--- app.js
+++ app.js
@@ -2,4 +2,15 @@
 
 // var h1 = React.createElement('h1', { "className": 'success' }, 'Hello World!');
 // ReactDOM.render(h1, document.getElementById('app'));
-ReactDOM.render(<h1>sdf</h1>, document.getElementById('app'));
+const Layout = React.createClass({
+  render() {
+    return <div className="overlay">
+      <div className="box">
+        <h2>title</h2>
+      </div>
+
+      <footer>this is footer.</footer>
+    </div>;
+  }
+});
+ReactDOM.render(<Layout />, document.getElementById('app'));
